21ded59c7db2d511a12d861b066a40d6c4025909
Jason <jason@invexi.com>
2017-08-15 08:55:37 -0700

    updated ipmort script

--- bin/import.sh
+++ bin/import.sh
@@ -5,12 +5,15 @@ runJob="time php $(dirname $0)/run-job.php"
 
 echo "Running ImportGeoJson"
 $runJob ImportGeoJson
+echo ""
 
 echo "Running ImportMembers"
 $runJob ImportMembers
+echo ""
 
 echo "Running ImportHeadshots"
 $runJob ImportHeadshots
+echo ""
 
 
 exit 0
