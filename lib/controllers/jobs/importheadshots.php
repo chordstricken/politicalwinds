2c7756ff50e27c715e811aac5f387f7398c274cb
Jason <jason@invexi.com>
2017-08-15 09:35:49 -0700

    updated headshot import to use git

--- lib/controllers/jobs/importheadshots.php
+++ lib/controllers/jobs/importheadshots.php
@@ -5,6 +5,7 @@ namespace controllers\jobs;
 use \Exception;
 use core\CURL;
 use core\Debug;
+use core\Git;
 use models\Member;
 
 /**
@@ -14,65 +15,32 @@ use models\Member;
  */
 class ImportHeadshots extends \core\Job {
 
-    private $total = 0;
-    private $i     = 0;
-
     /**
      * Main work function
      */
     protected function doWork() {
         try {
+            $result = Git::pull('https://github.com/unitedstates/images', '/unitedstates/images');
+            Debug::info($result);
 
-            if (!$index = file_get_contents(ROOT . '/api/static/us/current.json'))
-                throw new Exception('us/current.json not found');
+            $apiPath       = ROOT . '/api/static/members/photos';
+            $repoImagePath = ROOT . '/repos/unitedstates/images/congress/225x275';
+            $repoImages    = scandir($repoImagePath);
 
-            if (!$index = json_decode($index))
-                throw new Exception('Failed to decode JSON.' . json_last_error_msg());
+            foreach ($repoImages as $image) {
+                if ($image[0] === '.') continue;
 
-            $this->total = count(get_object_vars($index));
+                $dest = "$apiPath/$image[0]/$image";
+                if (!is_dir(dirname($dest)))
+                    mkdir(dirname($dest), 0777, true);
 
-            foreach ($index as $bioguide => $mData)
-                $this->acquireMemberHeadshot($bioguide);
+                copy("$repoImagePath/$image", $dest);
+            }
+
 
         } catch (Exception $e) {
             Debug::error(__METHOD__ . ': ' . $e->getMessage());
         }
-    }
-
-    private function acquireMemberHeadshot($id) {
-        $this->i++;
-
-        try {
-            $imgPath = "$id[0]/$id.jpg";
-            $imgFile = ROOT . "/api/static/members/photos/$imgPath";
-            $imgDir  = dirname($imgFile);
-
-            if (file_exists($imgFile)) {
-                Debug::info("Image already stored. $id ($this->i / $this->total)");
-                return;
-            }
-
-            $curl    = CURL::new("http://bioguide.congress.gov/bioguide/photo/$imgPath");
-            $img     = $curl->exec();
-
-            if ($curl->code() < 400) {
-
-                @mkdir($imgDir, 0777, true);
-
-                if (!file_put_contents($imgFile, $img))
-                    throw new Exception("Failed to save file $imgFile");
-
-                Debug::info("Acquired Headshot $id ($this->i / $this->total)");
-
-            } else {
-                Debug::info("Failed to get Headshot $id ($this->i / $this->total)");
-
-            }
-
-        } catch (Exception $e) {
-            Debug::error(__METHOD__ . ': ' . $e->getMessage());
-        }
-
     }
 
 }
